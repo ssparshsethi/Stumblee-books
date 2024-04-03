8dbcf236f96d1a20b356084aea06a0e9521adb8a
ssparshsethi <142225493+ssparshsethi@users.noreply.github.com>
2024-04-03 21:28:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function next(){
+    window.location="index2.html"
+}
+
+function Later(){
+    window.location="index.html"
+}
+function upcoming(){
+    window.location="indexMit.html"
+} 
+
+
+
